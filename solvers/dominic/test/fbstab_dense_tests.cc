ff134d80c463e4ec4a13e259334cc8dd7075ea48
Dominic Liao-McPherson <>
2019-01-24 19:23:04 -0500

    Passing component unit tests

--- solvers/dominic/test/fbstab_dense_tests.cc
+++ solvers/dominic/test/fbstab_dense_tests.cc
@@ -101,12 +101,171 @@ GTEST_TEST(FBstabDense, DenseVariable) {
 
 
 GTEST_TEST(FBstabDense, DenseResidual) {
-	
+	// create data object
+	double H[] = {3,1,1,1};
+	double f[] = {1,6};
+	double A[] = {-1,0,0,1};
+	double b[] = {0,-1};
+	int n = 2;
+	int q = 2;
+
+	QPsize size = {n,q};
+	DenseData data(H,f,A,b,size);
+
+	// create variables
+	DenseVariable x(size);
+	DenseVariable xbar(size);
+
+	x.LinkData(&data);
+	xbar.LinkData(&data);
+
+	x.z(0) = 1; x.z(1) = 5;
+	x.v(0) = 0.4; x.v(1) = 2;
+	x.InitConstraintMargin();
+
+	xbar.z(0) = -5; xbar.z(1) = 6;
+	xbar.v(0) = -9; xbar.v(1) = 1;
+	xbar.InitConstraintMargin();
+
+	DenseResidual r(size);
+	r.LinkData(&data);
+
+	double sigma = 0.5;
+	double alpha = 0.95;
+	r.alpha = alpha;
+
+	// PFB residual calculation
+	r.FBresidual(x,xbar,sigma);
+
+	double rz_expected[] = {11.6,13.5};
+	double rv_expected[] = {0.480683041678573,-8.88473245759182};
+
+	for(int i = 0;i<n;i++){
+		EXPECT_NEAR(r.rz(i),rz_expected[i],1e-14);
+	}
+	for(int i =0;i<q;i++){
+		EXPECT_NEAR(r.rv(i),rv_expected[i],1e-14);
+	}
+
+	// Natural residual calculation
+	r.NaturalResidual(x);
+
+	rz_expected[0] = 8.6;
+	rz_expected[1] = 14.0;
+	rv_expected[0] = 0.4;
+	rv_expected[1] = -6;
+
+	for(int i = 0;i<n;i++){
+		EXPECT_NEAR(r.rz(i),rz_expected[i],1e-14);
+	}
+	for(int i =0;i<q;i++){
+		EXPECT_NEAR(r.rv(i),rv_expected[i],1e-14);
+	}
+
+	// Penalized Natural Residual calculation
+	r.PenalizedNaturalResidual(x);
+
+	rz_expected[0] = 8.6;
+	rz_expected[1] = 14.0;
+	rv_expected[0] = 0.4;
+	rv_expected[1] = -5.7;
+
+	for(int i = 0;i<n;i++){
+		EXPECT_NEAR(r.rz(i),rz_expected[i],1e-14);
+	}
+	for(int i =0;i<q;i++){
+		EXPECT_NEAR(r.rv(i),rv_expected[i],1e-14);
+	}
+
+	// check the norm function
+	EXPECT_NEAR(r.Norm(),22.144477369697360,1e-14);
 }
 
 
-// GTEST_TEST(FBstabDense, DenseLinearSolver) {
-	
+GTEST_TEST(FBstabDense, DenseLinearSolver) {
+	// create data object
+	double H[] = {3,1,1,1};
+	double f[] = {1,6};
+	double A[] = {-1,0,0,1};
+	double b[] = {0,-1};
+	int n = 2;
+	int q = 2;
+
+	QPsize size = {n,q};
+	DenseData data(H,f,A,b,size);
+
+	// create variables
+	DenseVariable x(size);
+	DenseVariable dx(size);
+
+	x.LinkData(&data);
+	dx.LinkData(&data);
+
+	x.z(0) = 1; x.z(1) = 5;
+	x.v(0) = 0.4; x.v(1) = 2;
+	x.InitConstraintMargin();
+
+	dx.z(0) = -5; dx.z(1) = 6;
+	dx.v(0) = -9; dx.v(1) = 1;
+	dx.InitConstraintMargin();
+
+	DenseResidual r(size);
+	r.LinkData(&data);
+
+	double sigma = 0.5;
+	double alpha = 0.95;
+	r.alpha = alpha;
+
+	// PFB residual calculation
+	r.PenalizedNaturalResidual(x);
+
+	// create the linear solver object
+	DenseLinearSolver solver(size);
+	solver.LinkData(&data);
+
+	// test factor
+	solver.Factor(x,dx,sigma);
+
+	double mus_exp[] = {1.17966217107061,1.54674596622464};
+	double gamma_exp[] = {0.0223305769546052,1.84280375231402};
+
+	for(int i = 0;i<q;i++){
+		EXPECT_NEAR(solver.mus.data[i],mus_exp[i],1e-14);
+		EXPECT_NEAR(solver.gamma.data[i],gamma_exp[i],1e-14);
+	}
+
+	StaticMatrix K(solver.K);
+
+	double expected_K[] = {1.87588102960501,0.533082847055906,0,1.55152490683952};
+
+	// K need its upper triangle zeroed out for the comparison
+	// by default its left unchanged
+	K.tril();
+	for(int i = 0;i<n;i++){
+		EXPECT_NEAR(K.data[i],expected_K[i],1e-14);
+	}
+
+	r.Negate();
+	// now the solve step
+	solver.Solve(r,&dx);
+
+	double dz_exp[] = {-0.752407327230274,-6.29141168496996};
+	double dv_exp[] = {-0.324837330275923,-3.81047514531479};
+	double dy_exp[] = {-0.752407327230274,5.29141168496996};
+
+	for(int i = 0;i<n;i++){
+		EXPECT_NEAR(dx.z(i),dz_exp[i],1e-14);
+	}
+
+	for(int i = 0;i<q;i++){
+		EXPECT_NEAR(dx.v(i),dv_exp[i],1e-14);
+		EXPECT_NEAR(dx.y(i),dy_exp[i],1e-14);
+	}	
+}
+
+// GTEST_TEST(FBstabDense,SolverIntegration) {
+// 	double zopt_exp[] = {2.49999999664944, -8.49999998818445};
+// 	double vopt_exp[] = {3.65900262042578e-09,6.31591394316009e-11};
 // }
 
 
