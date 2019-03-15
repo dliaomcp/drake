145229062a392ee2d9fde22b2bce3773f23a1a64
Dominic Liao-McPherson <>
2019-03-15 17:37:20 -0400

    Constructor and destructor for the ricatti solver

--- solvers/dominic/components/ricatti_linear_solver.h
+++ solvers/dominic/components/ricatti_linear_solver.h
@@ -24,29 +24,163 @@ class RicattiLinearSolver{
  	bool Factor(const MSVariable &x, const MSVariable &xbar, double sigma);
  	bool Solve(const MSResidual &r, MSVariable *dx);
 
+ 	double alpha_ = 0.95;
  private:
  	int N_, nx_, nu_, nc_;
  	int nz_, nl_, nv_;
 
- 	double alpha_ = 0.95;
  	MPCData *data_ = nullptr;
  	double zero_tol_ = 1e-13;
+ 	bool memory_allocated_ = false;
 
  	MatrixSequence Q_, R_, S_;
  	MatrixSequence P_;
- 	MatrixSequence Sig_;
+ 	MatrixSequence SG_;
  	MatrixSequence M_;
  	MatrixSequence L_;
  	MatrixSequence SM_;
  	MatrixSequence AM_;
 
  	MatrixSequence h_;
- 	MatrixSequence theta_;
+ 	MatrixSequence th_;
+
+ 	StaticMatrix gamma_; 
+ 	StaticMatrix mus_;
+ 	StaticMatrix Gamma_;
+
+ 	StaticMatrix Linv_;
+ 	StaticMatrix r1_;
+ 	StaticMatrix r2_;
+
+ 	StaticMatrix rx_,rl_,ru_,rxp_,rlp_;
 
  	Point2D PFBgrad(double a, double b, double sigma);
 };
 
-DenseLinearSolver::Point2D DenseLinearSolver::PFBgrad(double a,
+RicattiLinearSolver::RicattiLinearSolver(QPsizeMPC size){
+	// compute sizes
+	N_ = size.N;
+	nx_ = size.nx;
+	nu_ = size.nu;
+	nc_ = size.nc;
+
+	nz_ = (N_+1)*(nx_+nu_);
+	nl_ = (N_+1)*(nx_);
+	nv_ = (N_+1)*(nc_);
+
+	int N = N_; int nx = nx_; int nu = nu_; int nc = nc_;
+	int nz = nz_; int nl = nl_; int nv = nv_;
+
+	int NN = N+1;
+
+	double *Q_mem = new double[nx*nx*NN];
+	double *S_mem = new double[nx*nu*NN];
+	double *R_mem = new double[nu*nu*NN];
+
+	double *P_mem = new double[nx*nu*NN];
+	double *SG_mem = new double[nu*nu*NN];
+	double *M_mem = new double[nx*nx*NN];
+	double *L_mem = new double[nx*nx*NN];
+	double *SM_mem = new double[nu*nx*NN];
+	double *AM_mem = new double[nx*nx*NN];
+
+	double *h_mem = new double[nx*1*NN];
+	double *th_mem = new double[nx*1*NN];
+
+	double *gamma_mem = new double[nv];
+	double *Gamma_mem = new double[nv];
+	double *mus_mem = new double[nv];
+
+	double *Linv_mem = new double[nx*nx];
+	double *r1_mem = new double[nz];
+	double *r2_mem = new double[nl];
+
+	double *rx_mem = new double[nx];
+	double *ru_mem = new double[nu];
+	double *rl_mem = new double[nx];
+	double *rxp_mem new double[nx];
+	double *rlp_mem = new double[nx];
+
+	Q_ = MatrixSequence(Qmem,N+1,nx,nx);
+	S_ = MatrixSequence(Smem,N+1,nu,nx);
+	R_ = MatrixSequence(Rmem,N+1,nu,nu);
+
+	P_ = MatrixSequence(Pmem,N+1,nx,nu);
+	SG_ = MatrixSequence(SGmem,N+1,nu,nu);
+	M_ = MatrixSequence(Mmem,N+1,nx,nx);
+	L_ = MatrixSequence(Lmem,N+1,nx,nx);
+	SM_ = MatrixSequence(SMmem,N+1,nu,nx);
+	AM_ = MatrixSequence(AMmem,N+1,nx,nx);
+
+	h_ = MatrixSequence(hmem,N+1,nx,1);
+	th_ = MatrixSequence(thmem,N+1,nx,1);
+
+	gamma_ = StaticMatrix(gamma_mem,nv,1);
+	mus_ = StaticMatrix(mus_mem,nv,1);
+	Gamma_ = StaticMatrix(Gamma_mem,nv,1);
+
+	Linv_ = StaticMatrix(Linv_mem,nx,nx);
+	r1_ = StaticMatrix(r1_mem,nz,1);
+	r2_ = StaticMatrix(r2_mem,nl,1);
+
+	rx_ = StaticMatrix(rx_mem,nx,1);
+	ru_ = StaticMatrix(ru_mem,nu,1);
+	rl_ = StaticMatrix(rl_mem,nx,1);
+	rxp_ = StaticMatrix(rxp_mem,nx,1);
+	rlp_ = StaticMatrix(rlp_mem,nx,1);
+
+	memory_allocated_ = true;
+
+}
+
+RicattiLinearSolver::~RicattiLinearSolver(){
+	if(memory_allocated_){
+		Q_.DeleteMemory();
+		R_.DeleteMemory();
+		S_.DeleteMemory();
+
+		P_.DeleteMemory();
+		SG_.DeleteMemory();
+		M_.DeleteMemory();
+		SM_.DeleteMemory();
+		AM_.DeleteMemory();
+
+		h_.DeleteMemory();
+		th_.DeleteMemory();
+
+		delete[] gamma_.data;
+		delete[] mus_.data;
+		delete[] Gamma_.data;
+
+		delete[] Linv_.data;
+		delete[] r1_.data;
+		delete[] r2_.data;
+
+		delete[] rx_.data;
+		delete[] ru_.data;
+		delete[] rl_.data;
+
+		delete[] rxp_.data;
+		delete[] rlp_.data;
+	}
+}
+
+void RicattiLinearSolver::LinkData(MPCData *data){
+	data_ = data;
+}
+
+bool RicattiLinearSolver::Factor(const MSVariable&x, const MSVariable &xbar, double sigma){
+	int N = N_; int nx = nx_; int nu = nu_; int nc = nc_;
+
+	// compute the barrier terms
+
+}
+
+bool RicattiLinearSolver::Solve(const MSResidual &r, MSVariable *dx){
+
+}
+
+RicattiLinearSolver::Point2D DenseLinearSolver::PFBgrad(double a,
  double b, double sigma){
 	double y = 0;
 	double x = 0;
