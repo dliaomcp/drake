987b19e35fe7702a6d25bba9e3256458c8f01471
Dominic Liao-McPherson <dliaomcp@umich.edu>
2019-07-03 11:33:54 -0400

    run clang-format on test and algorithm folders

--- solvers/fbstab/test/dense_component_unit_tests.cc
+++ solvers/fbstab/test/dense_component_unit_tests.cc
@@ -8,39 +8,38 @@ namespace solvers {
 namespace fbstab {
 namespace test {
 
-/** 
+/**
  * @file Runs unit tests for the Dense MPC components. See
  * dense_component_unit_tests.h for documentation.
  */
 GTEST_TEST(FBstabDense, DenseVariable) {
-	DenseComponentUnitTests test;
-	test.DenseVariableTests();
+  DenseComponentUnitTests test;
+  test.DenseVariableTests();
 }
 
 GTEST_TEST(FBstabDense, TestInnerResidualCalculation) {
-	DenseComponentUnitTests test;
-	test.InnerResidualCalculation();
+  DenseComponentUnitTests test;
+  test.InnerResidualCalculation();
 }
 
 GTEST_TEST(FBstabDense, NaturalResidualCalculation) {
-	DenseComponentUnitTests test;
-	test.NaturalResidualCalculation();
+  DenseComponentUnitTests test;
+  test.NaturalResidualCalculation();
 }
 
 GTEST_TEST(FBstabDense, DenseLinearSolver) {
-	DenseComponentUnitTests test;
-	test.LinearSolverResidual();
+  DenseComponentUnitTests test;
+  test.LinearSolverResidual();
 }
 
 GTEST_TEST(FBstabDense, InfeasibilityDetection) {
-	DenseComponentUnitTests test;
-	test.InfeasibilityDetection();
-
+  DenseComponentUnitTests test;
+  test.InfeasibilityDetection();
 }
 
 GTEST_TEST(FBstabDense, UnboundednessDetection) {
-	DenseComponentUnitTests test;
-	test.UnboundednessDetection();
+  DenseComponentUnitTests test;
+  test.UnboundednessDetection();
 }
 
 }  // namespace test
