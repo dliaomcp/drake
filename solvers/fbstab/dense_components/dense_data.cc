c5aef3c5ec153ee969bd128ba1e8288e49c58616
Dominic Liao-McPherson <dliaomcp@umich.edu>
2019-07-03 11:26:49 -0400

    run clang-format on dense_components

--- solvers/fbstab/dense_components/dense_data.cc
+++ solvers/fbstab/dense_components/dense_data.cc
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <Eigen/Dense>
 
-
 namespace drake {
 namespace solvers {
 namespace fbstab {
@@ -11,27 +10,30 @@ namespace fbstab {
 using MatrixXd = Eigen::MatrixXd;
 using VectorXd = Eigen::VectorXd;
 
-DenseData::DenseData(const MatrixXd* H,const VectorXd* f, const MatrixXd* A,const VectorXd* b){
-	if(H == nullptr || f == nullptr || A == nullptr || b == nullptr){
-		throw std::runtime_error("Inputs to DenseData::DenseData cannot be null.");
-	}
-	if(H->rows() != H->cols() || H->rows() != f->size()){
-		throw std::runtime_error("In DenseData::DenseData: H must be square and the same size as f.");
-	}
-	if(A->cols() != H->rows() || A->rows() != b->size()){
-		throw std::runtime_error("In DenseData::DenseData: Sizing of data defining Az <= b is inconsistent.");
-	}
-	
-	nz_ = f->size();
-	nv_ = b->size();
+DenseData::DenseData(const MatrixXd* H, const VectorXd* f, const MatrixXd* A,
+                     const VectorXd* b) {
+  if (H == nullptr || f == nullptr || A == nullptr || b == nullptr) {
+    throw std::runtime_error("Inputs to DenseData::DenseData cannot be null.");
+  }
+  if (H->rows() != H->cols() || H->rows() != f->size()) {
+    throw std::runtime_error(
+        "In DenseData::DenseData: H must be square and the same size as f.");
+  }
+  if (A->cols() != H->rows() || A->rows() != b->size()) {
+    throw std::runtime_error(
+        "In DenseData::DenseData: Sizing of data defining Az <= b is "
+        "inconsistent.");
+  }
 
-	H_ = H;
-	A_ = A;
-	f_ = f;
-	b_ = b;
+  nz_ = f->size();
+  nv_ = b->size();
+
+  H_ = H;
+  A_ = A;
+  f_ = f;
+  b_ = b;
 }
 
 }  // namespace fbstab
 }  // namespace solvers
 }  // namespace drake
-
--- solvers/fbstab/dense_components/dense_residual.cc
+++ solvers/fbstab/dense_components/dense_residual.cc
@@ -1,122 +1,121 @@
 #include "drake/solvers/fbstab/dense_components/dense_residual.h"
 
 #include <cmath>
-#include <Eigen/Dense>
 #include <iostream>
+#include <Eigen/Dense>
 
-#include "drake/solvers/fbstab/dense_components/dense_variable.h"
 #include "drake/solvers/fbstab/dense_components/dense_data.h"
+#include "drake/solvers/fbstab/dense_components/dense_variable.h"
 
 namespace drake {
 namespace solvers {
 namespace fbstab {
 
-DenseResidual::DenseResidual(int nz, int nv){
-	nz_ = nz;
-	nv_ = nv;
-	z_.resize(nz_);
-	v_.resize(nv_);
+DenseResidual::DenseResidual(int nz, int nv) {
+  nz_ = nz;
+  nv_ = nv;
+  z_.resize(nz_);
+  v_.resize(nv_);
 }
 
-void DenseResidual::Negate(){
-	z_ *= -1.0;
-	v_ *= -1.0;
+void DenseResidual::Negate() {
+  z_ *= -1.0;
+  v_ *= -1.0;
 }
 
-void DenseResidual::NaturalResidual(const DenseVariable& x){
-	if(data_ == nullptr){
-		throw std::runtime_error("DenseResidual::NaturalResidual cannot be used unless data is linked");
-	}
-	// rz = H*z + f + A'*v
-	// calls are arranged to avoid creating temporaries
-	z_.noalias() = data_->H() * x.z();
-	z_.noalias() += data_->f();
-	z_.noalias() += data_->A().transpose()*x.v();
+void DenseResidual::NaturalResidual(const DenseVariable& x) {
+  if (data_ == nullptr) {
+    throw std::runtime_error(
+        "DenseResidual::NaturalResidual cannot be used unless data is linked");
+  }
+  // rz = H*z + f + A'*v
+  // calls are arranged to avoid creating temporaries
+  z_.noalias() = data_->H() * x.z();
+  z_.noalias() += data_->f();
+  z_.noalias() += data_->A().transpose() * x.v();
 
-	// rv = min(y,v)
-	v_.noalias() = x.y().cwiseMin(x.v());
+  // rv = min(y,v)
+  v_.noalias() = x.y().cwiseMin(x.v());
 
-	znorm_ = z_.norm();
-	vnorm_ = v_.norm();
+  znorm_ = z_.norm();
+  vnorm_ = v_.norm();
 }
 
-void DenseResidual::PenalizedNaturalResidual(const DenseVariable& x){
-	if(data_ == nullptr){
-		throw std::runtime_error("DenseResidual::PenalizedNaturalResidual cannot be used unless data is linked");
-	}
-	// rz = H*z + f + A'*v
-	// calls are arranged to avoid creating temporaries
-	z_.noalias() = data_->H() * x.z();
-	z_.noalias() += data_->f();
-	z_.noalias() += data_->A().transpose()*x.v();
+void DenseResidual::PenalizedNaturalResidual(const DenseVariable& x) {
+  if (data_ == nullptr) {
+    throw std::runtime_error(
+        "DenseResidual::PenalizedNaturalResidual cannot be used unless data is "
+        "linked");
+  }
+  // rz = H*z + f + A'*v
+  // calls are arranged to avoid creating temporaries
+  z_.noalias() = data_->H() * x.z();
+  z_.noalias() += data_->f();
+  z_.noalias() += data_->A().transpose() * x.v();
 
-	// rv = min(y,v) + max(0,y)*max(0,v)
-	for(int i = 0; i < nv_; i++){
-		v_(i) = min(x.y()(i),x.v()(i));
-		v_(i) = alpha_*v()(i) + (1.0-alpha_)*max(0.0,x.y()(i))*max(0.0,x.v()(i));
-	}
+  // rv = min(y,v) + max(0,y)*max(0,v)
+  for (int i = 0; i < nv_; i++) {
+    v_(i) = min(x.y()(i), x.v()(i));
+    v_(i) = alpha_ * v()(i) +
+            (1.0 - alpha_) * max(0.0, x.y()(i)) * max(0.0, x.v()(i));
+  }
 
-	znorm_ = z_.norm();
-	vnorm_ = v_.norm();
+  znorm_ = z_.norm();
+  vnorm_ = v_.norm();
 }
 
-void DenseResidual::InnerResidual(const DenseVariable& x, const DenseVariable& xbar, double sigma){
-	if(data_ == nullptr){
-		throw std::runtime_error("DenseResidual::InerResidual cannot be used unless data is linked");
-	}
-	// rz = Hz + f + A'v + sigma(z - zbar)
-	// calls are arranged to avoid creating temporaries
-	z_.noalias() = data_->H() * x.z();
-	z_.noalias() += data_->f();
-	z_.noalias() += data_->A().transpose()*x.v();
-	z_.noalias() += sigma*(x.z() - xbar.z());
+void DenseResidual::InnerResidual(const DenseVariable& x,
+                                  const DenseVariable& xbar, double sigma) {
+  if (data_ == nullptr) {
+    throw std::runtime_error(
+        "DenseResidual::InerResidual cannot be used unless data is linked");
+  }
+  // rz = Hz + f + A'v + sigma(z - zbar)
+  // calls are arranged to avoid creating temporaries
+  z_.noalias() = data_->H() * x.z();
+  z_.noalias() += data_->f();
+  z_.noalias() += data_->A().transpose() * x.v();
+  z_.noalias() += sigma * (x.z() - xbar.z());
 
-	// v_ = phi(ys,v), ys = y + sigma(x.v - xbar.v)
-	for(int i = 0; i < nv_; i++){
-		double ys = x.y()(i) + sigma*(x.v()(i) - xbar.v()(i));
-		v_(i) = pfb(ys,x.v()(i),alpha_);
-	}
+  // v_ = phi(ys,v), ys = y + sigma(x.v - xbar.v)
+  for (int i = 0; i < nv_; i++) {
+    double ys = x.y()(i) + sigma * (x.v()(i) - xbar.v()(i));
+    v_(i) = pfb(ys, x.v()(i), alpha_);
+  }
 
-	znorm_ = z_.norm();
-	vnorm_ = v_.norm();
+  znorm_ = z_.norm();
+  vnorm_ = v_.norm();
 }
 
-void DenseResidual::Copy(const DenseResidual &x){
-	if(nz_ != x.nz_ || nv_ != x.nv_){
-		throw std::runtime_error("Size mismatch in DenseResidual::Copy");
-	}
-	z_ = x.z_;
-	v_ = x.v_;
+void DenseResidual::Copy(const DenseResidual& x) {
+  if (nz_ != x.nz_ || nv_ != x.nv_) {
+    throw std::runtime_error("Size mismatch in DenseResidual::Copy");
+  }
+  z_ = x.z_;
+  v_ = x.v_;
 }
 
-void DenseResidual::Fill(double a){
-	z_.fill(a);
-	v_.fill(a);
+void DenseResidual::Fill(double a) {
+  z_.fill(a);
+  v_.fill(a);
 }
 
-double DenseResidual::Norm() const{
-	return znorm_ + vnorm_;
+double DenseResidual::Norm() const { return znorm_ + vnorm_; }
+
+double DenseResidual::Merit() const {
+  double temp = Norm();
+  return 0.5 * temp * temp;
 }
 
-double DenseResidual::Merit() const{
-	double temp = Norm();
-	return 0.5*temp*temp;
-}
+double DenseResidual::max(double a, double b) { return a > b ? a : b; }
 
-double DenseResidual::max(double a, double b){
-	return a>b ? a : b;
-}
+double DenseResidual::min(double a, double b) { return a < b ? a : b; }
 
-double DenseResidual::min(double a, double b){
-	return a<b ? a : b;
-}
-
-double DenseResidual::pfb(double a, double b, double alpha){
-	double fb = a + b - sqrt(a*a + b*b);
-	return alpha * fb + (1.0-alpha)* max(0,a)*max(0,b);
+double DenseResidual::pfb(double a, double b, double alpha) {
+  double fb = a + b - sqrt(a * a + b * b);
+  return alpha * fb + (1.0 - alpha) * max(0, a) * max(0, b);
 }
 
 }  // namespace fbstab
 }  // namespace solvers
 }  // namespace drake
-
