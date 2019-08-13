ce5cbb46ce2f66111521f33c1b5236e8efbe829e
Dominic Liao-McPherson <dliaomcp@umich.edu>
2019-08-12 21:09:51 -0400

    Draft of MPC assembly in Mathematical Program

--- solvers/fbstab/test/scaling_benchmarks_main.cc
+++ solvers/fbstab/test/scaling_benchmarks_main.cc
@@ -6,20 +6,68 @@
 #include <Eigen/Dense>
 
 #include "drake/solvers/fbstab/test/solver_wrappers.h"
+#include "drake/solvers/mathematical_program.h"
 
 using string = std::string;
 namespace test = drake::solvers::fbstab::test;
+using MatrixXd = Eigen::MatrixXd;
+using VectorXd = Eigen::VectorXd;
+using expr = drake::symbolic::Expression;
 
 int main(void) {
   // Scaling horizon.
-  Eigen::VectorXi N(4);
-  N << 2, 5, 10, 100;
+  // Eigen::VectorXi N(4);
+  // N << 2, 5, 10, 100;
 
-  string example = "ServoMotor";
-  test::ScalingBenchmark<test::FBstabWrapper> s(N, example);
-  s.RunTiming();
+  // string example = "ServoMotor";
+  // test::ScalingBenchmark<test::FBstabWrapper> s(N, example);
+  // s.RunTiming();
 
-  std::cout << s.WriteResultsToFile("test.csv");
+  // std::cout << s.WriteResultsToFile("test.csv");
+
+  drake::solvers::MathematicalProgram mp;
+  auto x = mp.NewContinuousVariables(10, "x");
+  auto u = mp.NewContinuousVariables(10, "u");
+
+  // std::cout << x << std::endl;
+
+  auto x1 = x.segment(0, 3);
+  auto xsym = x1.cast<drake::symbolic::Expression>();
+
+  auto x2 = u.segment(0, 3);
+
+  drake::solvers::VectorXDecisionVariable z(6);
+  z << x1, x2;
+  std::cout << z << std::endl;
+
+  MatrixXd Q = MatrixXd::Ones(3, 3);
+  VectorXd d = VectorXd::Zero(3);
+
+  auto q1 = mp.AddQuadraticCost(Q, d, x1);
+
+  // std::cout << q1.evaluator()->Q() << std::endl;
+  // std::cout << q1.variables() << std::endl;
+
+  MatrixXd A = MatrixXd::Identity(3, 3);
+
+  auto tt = x1.cast<expr>() - A.cast<expr>() * x2.cast<expr>();
+  std::cout << tt << std::endl;
+
+  auto l1 = mp.AddLinearEqualityConstraint(tt, d);
+  std::cout << l1.evaluator()->A() << std::endl;
+
+  MatrixXd E = MatrixXd::Ones(4, 3);
+  MatrixXd L = -MatrixXd::Ones(4, 3);
+
+  MatrixXd G(4, 6);
+  VectorXd p = VectorXd::Ones(4) * 7;
+  G << E, L;
+  // std::cout << G << std::endl;
+
+  auto lin = mp.AddLinearConstraint((G * z).array() <= p.array());
+  std::cout << lin.evaluator()->A() << std::endl;
+  std::cout << lin.evaluator()->upper_bound() << std::endl;
+  std::cout << lin.evaluator()->lower_bound() << std::endl;
 
   return 0;
 }
--- solvers/fbstab/test/solver_wrappers.h
+++ solvers/fbstab/test/solver_wrappers.h
@@ -6,6 +6,7 @@
 #include <Eigen/Dense>
 
 #include "drake/solvers/fbstab/fbstab_mpc.h"
+#include "drake/solvers/mathematical_program.h"
 
 namespace drake {
 namespace solvers {
@@ -32,6 +33,9 @@ struct WrapperOutput {
 // TODO(dliaomcp@umich.edu) write an abstract wrapper class to define the
 // interface.
 
+using MatrixXd = Eigen::MatrixXd;
+using VectorXd = Eigen::VectorXd;
+
 /**
  * This class wraps FBstab and implements a standardized interface to enable
  * benchmarking.
@@ -39,7 +43,7 @@ struct WrapperOutput {
 class FBstabWrapper {
  public:
   FBstabWrapper(FBstabMpc::QPData data) {
-    // Extract the size data.
+    // Extract size data.
     const int N = data.B->size();
     const int nx = data.B->at(0).rows();
     const int nu = data.B->at(0).cols();
@@ -118,13 +122,76 @@ class FBstabWrapper {
   Eigen::VectorXd y_;
 };
 
+class MosekWrapper {
+ public:
+  MosekWrapper(FBstabMpc::QPData data) {
+    // Extract size data.
+    const int N = data.B->size();
+    const int nx = data.B->at(0).rows();
+    const int nu = data.B->at(0).cols();
+    const int nc = data.E->at(0).rows();
+
+    // Set up the mathematical program.
+    auto x = mp_.NewContinuousVariables((N + 1) * nx, "x");
+    auto u = mp_.NewContinuousVariables((N + 1) * nu, "u");
+
+    // Add linear-quadratic cost and inequality constraints.
+    MatrixXd H(nx + nu, nx + nu);
+    MatrixXd G(nc, nx + nu);
+    VectorXd f(nx + nu);
+    VectorXd d(nc);
+
+    for (int i = 0; i <= N; i++) {
+      // Get the variables of interest
+      auto xi = x.segment(i * nx, nx);
+      auto ui = u.segment(i * nu, nu);
+
+      VectorXDecisionVariable z(nx + nu);
+      z << xi, ui;
+
+      // Cost 0.5 z'Hz + f'z
+      H << data.Q->at(i), data.S->at(i).transpose(), data.S->at(i),
+          data.R->at(i);
+      f << data.q->at(i), data.r->at(i);
+
+      mp_.AddQuadraticCost(H, f, z);
+
+      // Inequalities Ex + Lu <= d
+      G << data.E->at(i), data.L->at(i);
+      d = -data.d->at(i);
+
+      mp_.AddLinearConstraint((G * z).array() <= d.array());
+    }
+
+    // Initial state constraint.
+    auto x0 = x.segment(0, nx);
+    mp_.AddLinearEqualityConstraint(MatrixXd::Identity(nx, nx), *data.x0, x0);
+
+    // Dynamic constraints
+    for (int i = 0; i < N; i++) {
+      auto xip1 = x.segment((i + 1) * nx, nx);
+      auto xi = x.segment(i * nx, nx);
+      auto ui = u.segment(i * nu, nu);
+
+      auto A = data.A->at(i).cast<symbolic::Expression>();
+      auto B = data.B->at(i).cast<symbolic::Expression>();
+      auto temp = xip1.cast<symbolic::Expression>() -
+                  A * xi.cast<symbolic::Expression>() -
+                  B * ui.cast<symbolic::Expression>();
+
+      mp_.AddLinearEqualityConstraint(temp, data.c->at(i));
+    }
+  }
+
+ private:
+  MathematicalProgram mp_;
+};
+
 // Wraps qpOASES
 class QpOasesWrapper {};
 
 // Wraps ECOS
 class EcosWrapper {};
-
-class MosekWrapper {};
 
 }  // namespace test
 }  // namespace fbstab
